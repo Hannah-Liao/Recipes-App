e45afc3a4d22cd2adddf9b7ce9413650b9b899fc
Hannah-Liao <hanna.liao@outlook.com>
2023-04-27 01:28:25 +0100

    deploy

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import * as path from 'path';
+// import * as path from 'path';
 
 import { userRouter } from './routes/users.js';
 import { recipeRouter } from './routes/recipes.js';
@@ -16,13 +16,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-const __dirname = path.dirname("")
-app.use(express.static(path.join(__dirname, '../client/build')));
+// const __dirname = path.dirname("")
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/buildindex.html'));
-});
-
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../client/buildindex.html'));
+// });
 
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/recipes", recipeRouter);
